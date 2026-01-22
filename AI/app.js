2b54bcd504af47cf1b48502478315d6c4a8f3b1b
Melwx <ss40sien@gmail.com>
2026-01-22 08:39:59 +0700

    Update app.js

--- AI/app.js
+++ AI/app.js
@@ -1,7 +1,7 @@
 // ====== cấu hình chỉnh trong code ======
 // Dán 5 API key của bạn vào đây:
 const API_KEYS = [
-  "AIzaSyDpFhjVWPFUkf27g1lNXNDLhPVY0t7ywt8",
+  "AIzaSyAIVwOr9eIQRy37pD7dKV_QUwpUnzm1rMg",
   "AIzaSyCqPYZMqK6_ykbMcHAK69V4Kb09X-YXxls",
   "AIzaSyBgRBxZEzi6dEXt4dj9f_qbg6zsm4aLVBg",
   "AIzaSyDGFt2b0IkLIf3I967GzX0q7GJs8o2x20k",
